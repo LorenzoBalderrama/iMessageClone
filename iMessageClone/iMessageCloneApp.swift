aefbb574066ed3fd8cd1f90510375c86671fa593
Zo <70714420+LorenzoBalderrama@users.noreply.github.com>
2023-06-09 23:55:15 -0600

    Initial Commit

---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by Lorenzo Porras on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
